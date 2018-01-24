fd0fae5bce7a2502140dc4326b0fc3866c2e464f
Svanurjoh <svanurjoh15@ru.is>
2018-01-24 09:40:18 +0000

    Setup and environment variables

---
+++ src/Helpers/Actions.java
@@ -0,0 +1,12 @@
+package Helpers;
+
+public class Actions {
+    public Actions(){}
+
+    public static final String TURN_ON = "TURN_ON";
+    public static final String TURN_OFF = "TURN_OFF";
+    public static final String GO = "GO";
+    public static final String TURN_LEFT = "TURN_LEFT";
+    public static final String TURN_RIGHT = "TURN_RIGHT";
+    public static final String SUCK = "SUCK";
+}
